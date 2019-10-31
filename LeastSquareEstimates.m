3f1af47b59505857510a9f6daa29d7331d3a34a0
Abhishek Mukherjee <51139088+amukher3@users.noreply.github.com>
2019-10-31 02:07:01 -0400

    Delete LeastSquareEstimates.m

--- LeastSquareEstimates.m
+++
@@ -1,8 +0,0 @@
-function [L] = LeastSquareEstimates(x,samples,T,TauOne,TauTwo)
-Lhood=0; 
-for i=1:length(samples)
-mu(i)= x(1)*(exp((x(2)-(i)*T)/TauOne) - exp((x(2)-(i)*T)/TauTwo));                       
-Lhood = Lhood + (samples(i)-mu(i))^2;
-end
-L= (Lhood); 
-end
